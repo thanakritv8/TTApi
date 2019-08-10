c0f39b67970dc04c63bfeec5c4e48feff23f5567
mooqpoom <poom_nura@hotmail.com>
2019-08-10 23:22:07 +0700

    update worksheet

--- TTApi/Controllers/WorksheetController.cs
+++ TTApi/Controllers/WorksheetController.cs
@@ -24,6 +24,7 @@ namespace TTApi.Controllers
         #region CreateWorksheet
 
         // POST CheckList/Profile/CreateWorksheet
+        // POST CheckList/Profile/InsertWorksheet
         /// <summary>
         /// var model = new FormData(); 
         /// model.append('tran_code', 'เลขที่ใบงาน');
@@ -37,7 +38,10 @@ namespace TTApi.Controllers
         /// model.append('driver_id_2', 'PK พขร 2');
         /// model.append('driver_id_3', 'PK พขร ฝึกหัด');
         /// model.append('license_id_head', 'PK รถหัวลาก');
-        /// model.append('license_id_tail', 'PK หางกึ่งพ่วง');
+        /// model.append('license_id_tail', 'PK หางกึ่งพ่วง'); 
+        /// model.append('sheet_name', 'ชื่อใบงาน');
+        /// model.append('cont1', 'เลขที่ตู้1');
+        /// model.append('cont2', 'เลขที่ตู้2');
         /// model.append('remark', 'หมายเหตุ');  
         /// ***ajax*** 
         /// 
@@ -55,10 +59,10 @@ namespace TTApi.Controllers
             using (SqlConnection con = hc.ConnectDatabase())
             {
                 string _SQL = "INSERT INTO [dbo].[transport] (tran_code,number_po  ,cus_id  ,branch_id  ,contact_id ,product_id  ,trunk_id " +
-                    ",driver_id_1  ,driver_id_2 ,driver_id_3  ,license_id_head  ,license_id_tail ,remark ,tran_status_id   ,create_by_user_id ) "+
+                    ",driver_id_1  ,driver_id_2 ,driver_id_3  ,license_id_head  ,license_id_tail ,remark ,tran_status_id ,create_by_user_id ,sheet_name ,cont1,cont2   ) " +
      " output inserted.tran_id VALUES (N'" + val.tran_code + "', N'" + val.number_po + "', '" + val.cus_id + "', '" + val.branch_id + "', '" + val.contact_id + "', '" + val.product_id + "'" +
            ", '" + val.trunk_id + "' ,'" + val.driver_id_1 + "' , '" + val.driver_id_2 + "', '" + val.driver_id_3 + "', '" + val.license_id_head + "' , '" + val.license_id_tail + "'" +
-           ", N'" + val.remark + "'  , '" + val.tran_status_id + "'  , 1 )";
+           ", N'" + val.remark + "'  , '" + val.tran_status_id + "'  , 1  , '" + val.sheet_name + "' , '" + val.cont1 + "', '" + val.cont2 + "')";
                 using (SqlCommand cmd = new SqlCommand(_SQL, con))
                 {
                     try
@@ -81,7 +85,145 @@ namespace TTApi.Controllers
             }
             return ecm;
         }
-#endregion
+
+        // POST CheckList/Profile/UpdateWorksheet
+        /// <summary>
+        /// อัพเดทWorksheet
+        /// </summary>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [Route("UpdateWorksheet")]
+        public ExecuteModels UpdateWorksheet(WorksheetModels val)
+        {
+            ExecuteModels ecm = new ExecuteModels();
+            HomeController hc = new HomeController();
+            using (SqlConnection con = hc.ConnectDatabase())
+            { 
+                string _SQL = "UPDATE  transport SET tran_code = N'" + val.tran_code + "'  , number_po = N'" + val.number_po + "' , cus_id = " + val.cus_id + " , branch_id = " + val.branch_id + " , contact_id = " + val.contact_id +
+     " , product_id = " + val.product_id + " , trunk_id = " + val.trunk_id + ", driver_id_1 = " + val.driver_id_1 + ", driver_id_2 = " + val.driver_id_2 + ", driver_id_3 = " + val.driver_id_3 + ", license_id_head = " + val.license_id_head + 
+     " , license_id_tail = " + val.license_id_tail + ", remark = " + val.remark + " , tran_status_id = " + val.tran_status_id +  ", update_by_user_id = 1 , sheet_name = N'" + val.sheet_name + "' , cont1 = N'" + val.cont1 + "', cont2 = N'" + val.cont2 + "' where tran_id = " + val.tran_id;
+ 
+                using (SqlCommand cmd = new SqlCommand(_SQL, con))
+                {
+                    try
+                    {
+                        if (Int32.Parse(cmd.ExecuteNonQuery().ToString()) == 1)
+                        {
+                            ecm.result = 0;
+                            ecm.code = "OK";
+                        }
+                        else
+                        {
+                            ecm.result = 1;
+                            ecm.code = _SQL;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ecm.result = 1;
+                        ecm.code = ex.Message;
+                    }
+                }
+                con.Close();
+            }
+            return ecm;
+        }
+
+        // POST CheckList/Profile/DeleteWorksheet
+        /// <summary>
+        /// ลบWorksheet
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [Route("DeleteWorksheet")]
+        public ExecuteModels DelWorksheet(WorksheetModels val)
+        {
+            ExecuteModels ecm = new ExecuteModels();
+            HomeController hc = new HomeController();
+            using (SqlConnection con = hc.ConnectDatabase())
+            {
+                string _SQL = "delete from transport where tran_id = " + val.tran_id;
+                using (SqlCommand cmd = new SqlCommand(_SQL, con))
+                {
+                    try
+                    {
+                        if (Int32.Parse(cmd.ExecuteNonQuery().ToString()) == 1)
+                        {
+                            ecm.result = 0;
+                            ecm.code = "OK";
+                        }
+                        else
+                        {
+                            ecm.result = 1;
+                            ecm.code = _SQL;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        ecm.result = 1;
+                        ecm.code = ex.Message;
+                    }
+                }
+                con.Close();
+            }
+            return ecm;
+        }
+
+        // POST CheckList/Profile/GetWorksheetAll
+        /// <summary>
+        /// เรียกดูข้อมูลWorksheet
+        /// </summary>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [Route("GetWorksheetAll")]
+        public List<WorksheetAllView> GetWorksheetAll()
+        {
+            HomeController hc = new HomeController();
+            List<WorksheetAllView> ul = new List<WorksheetAllView>();
+            using (SqlConnection con = hc.ConnectDatabase())
+            {
+                string _SQL = "select cus_id, cus_name from customer";
+                using (SqlCommand cmd = new SqlCommand(_SQL, con))
+                {
+                    DataTable _Dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(_Dt);
+                    da.Dispose();
+                    foreach (DataRow _Item in _Dt.Rows)
+                    {
+                        WorksheetAllView wsa = new WorksheetAllView(); 
+                        wsa.tran_id = _Item["tran_id"].ToString();
+                        wsa.tran_code = _Item["tran_code"].ToString();
+                        wsa.number_po = _Item["number_po"].ToString();
+                        wsa.cus_id = _Item["cus_id"].ToString();
+                        wsa.branch_id = _Item["branch_id"].ToString();
+                        wsa.contact_id = _Item["contact_id"].ToString();
+                        wsa.product_id = _Item["product_id"].ToString();
+                        wsa.trunk_id = _Item["trunk_id"].ToString();
+                        wsa.driver_id_1 = _Item["driver_id_1"].ToString();
+                        wsa.driver_id_2 = _Item["driver_id_2"].ToString();
+                        wsa.driver_id_3 = _Item["driver_id_3"].ToString();
+                        wsa.license_id_head = _Item["license_id_head"].ToString();
+                        wsa.license_id_tail = _Item["license_id_tail"].ToString();
+                        wsa.remark = _Item["remark"].ToString();
+                        wsa.tran_status_id = _Item["tran_status_id"].ToString();
+                        wsa.sheet_name = _Item["sheet_name"].ToString();
+                        wsa.cont1 = _Item["cont1"].ToString();
+                        wsa.cont2 = _Item["cont2"].ToString();
+                        wsa.create_date = _Item["create_date"].ToString();
+                        wsa.create_by_user_id = _Item["create_by_user_id"].ToString();
+                        wsa.update_date = _Item["update_date"].ToString();
+                        wsa.update_by_user_id = _Item["update_by_user_id"].ToString();
+                        ul.Add(wsa);
+                    }
+                }
+                con.Close();
+            }
+            return ul;
+        }
+ 
+        #endregion
 
     }
 }
--- TTApi/Models/WorksheetModels.cs
+++ TTApi/Models/WorksheetModels.cs
@@ -14,6 +14,29 @@ namespace TTApi.Models
 
     public class WorksheetModels
     {
+        public string tran_id { get; set; }
+        public string tran_code { get; set; }
+        public string number_po { get; set; }
+        public string cus_id { get; set; }
+        public string branch_id { get; set; }
+        public string contact_id { get; set; }
+        public string product_id { get; set; }
+        public string trunk_id { get; set; }
+        public string driver_id_1 { get; set; }
+        public string driver_id_2 { get; set; }
+        public string driver_id_3 { get; set; }
+        public string license_id_head { get; set; }
+        public string license_id_tail { get; set; }
+        public string remark { get; set; }
+        public string tran_status_id { get; set; } 
+        public string sheet_name { get; set; }
+        public string cont1 { get; set; }
+        public string cont2 { get; set; }
+    }
+     
+    public class WorksheetAllView
+    {
+        public string tran_id { get; set; }
         public string tran_code { get; set; }
         public string number_po { get; set; }
         public string cus_id { get; set; }
@@ -28,8 +51,15 @@ namespace TTApi.Models
         public string license_id_tail { get; set; }
         public string remark { get; set; }
         public string tran_status_id { get; set; }
+        public string sheet_name { get; set; }
+        public string cont1 { get; set; }
+        public string cont2 { get; set; }
+        public string create_date { get; set; }
+        public string create_by_user_id { get; set; }
+        public string update_date { get; set; }
+        public string update_by_user_id { get; set; }
+
     }
- 
- 
+
     #endregion
 }
